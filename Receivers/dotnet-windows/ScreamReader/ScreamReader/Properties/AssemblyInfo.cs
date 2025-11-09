1f5367c3e0b5270764129f92ec0f0319ef0edd4e
FarmVivi <farmvividev@gmail.com>
2025-11-09 19:37:52 +0100

    Update AssemblyInfo, project files, and add application manifest for ScreamReader

--- Receivers/dotnet-windows/ScreamReader/ScreamReader/Properties/AssemblyInfo.cs
+++ Receivers/dotnet-windows/ScreamReader/ScreamReader/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScreamReader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Low-latency network audio receiver for Scream virtual audio driver. Supports multicast/unicast UDP streaming with WASAPI output and adaptive buffering.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Scream Project")]
 [assembly: AssemblyProduct("ScreamReader")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017-2025 Scream Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
